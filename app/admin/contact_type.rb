379eccced08ba9adf57fbbec052cc8d603f2cd24
Prelang Builder <builder@prelang.com>
2016-02-29 19:30:16 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'ContactType' to allow for create, update

--- app/admin/contact_type.rb
+++ app/admin/contact_type.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register ContactType do
+  permit_params :contact_type
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
