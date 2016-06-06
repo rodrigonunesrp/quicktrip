5c60f60c45a61a1f0599e42db6d38fbea9616bff
Rodrigo Nunes de Souza <rodrigonunesrp@gmail.com>
2016-06-06 18:42:54 -0300

    adding more debug info into heroic logs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,9 +42,9 @@ Rails.application.configure do
   # config.force_ssl = true
 
   # Set to :debug to see everything in the log.
-  config.log_level = :debug
-  #config.logger = Logger.new(STDOUT) 
-  #config.logger.level = Logger::DEBUG
+  config.logger = Logger.new(STDOUT) 
+  config.logger.level = Logger::DEBUG
+  #config.log_level = :debug
 
 
 
