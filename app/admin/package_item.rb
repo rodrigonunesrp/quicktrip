61cc2bcda586ba5e33a66846023cf0762b32dc2a
Rodrigo Nunes de Souza <rodrigonunesrp@gmail.com>
2016-06-06 18:26:20 -0300

    new changes in admin package_item index show page

--- app/admin/package_item.rb
+++ app/admin/package_item.rb
@@ -2,10 +2,19 @@ ActiveAdmin.register PackageItem do
   permit_params :item, :link, :description, :package_id, :image, :pack_type
 
   	index do
-		column :package_id
+		column "Image" do |image| 
+			"<img src='#{image.image}' height='100px' width='150px' >".html_safe
+		end	
+		column "Pacote" do |package|
+			package.package.description
+		end
+		column "Tipo" do |package|
+			package.package.package_type.package_type
+		end
+		column "Abrir link como"do |package|
+			(package.pack_type == 1) ? "Abrir campo link em nova janela" : "Abrir show page do package_item e carregar link no iframe" 
+		end
 		column :item
-		column :pack_type
-		column image_tag :image
 		column :created_at
 		column :updated_at
 		actions
@@ -13,9 +22,9 @@ ActiveAdmin.register PackageItem do
 
 	form do |f|
 	  f.inputs do
-	    f.input :package_id, :label => 'Package', :as => :select, :collection => Package.all.map{|p| ["#{p.description}", p.id]}
+	    f.input :package_id, :label => 'Pacote', :as => :select, :collection => Package.all.map{|p| ["#{p.description}", p.id]}
 	    f.input :item
-	    f.input :pack_type, :label => 'Tipo do Pacote', :as => :select, :collection => {"Abrir campo link em nova janela" => 1, "Abrir show page do package_item e carregar link no iframe" => 2}
+	    f.input :pack_type, :label => 'Abrir link como', :as => :select, :collection => {"Abrir campo link em nova janela" => 1, "Abrir show page do package_item e carregar link no iframe" => 2}
 	    f.input :image
 	    f.input :link
 	    f.input :description
